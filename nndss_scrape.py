2c0066f3b1a0b704df0f8717f320ec78e9a3aee7
Daniel J Miller <danielmiller364@gmail.com>
2020-05-17 18:19:32 +1000

    Add data first time

---
+++ nndss_scrape.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import datetime
+from bs4 import BeautifulSoup
+import csv
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome('chromedriver',options=options)
+
+col_headings = ['YEAR', 'ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA', 'Aust', 'condition', 'updated']
+
+with open("count.csv", "w") as f:
+    wr = csv.writer(f)
+    wr.writerow(col_headings)
+
+driver.get("http://www9.health.gov.au/cda/source/rpt_4_sel.cfm")
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+causeList = {}
+table = soup.find('select')
+rows = table.find_all('option')
+
+for tag in rows:
+	idNo = tag.get('value')
+	cause = tag.text.strip()
+	causeList[idNo] = cause  
+
+for key in causeList:
+	report = driver.find_element_by_id("REPORT_OPTION_1")
+	report.click()
+	elem = driver.find_element_by_name("CAUSE")
+	elem.send_keys(causeList[key])
+	button = driver.find_element_by_name("CTIME1")
+	button.click()
+	time.sleep(3)
+
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	#table = driver.find_element_by_css_selector("#content > div:nth-child(1) > div > div > table")
+	table = soup.find('table')
+	#updated = driver.find_elements_by_class_name("publish-date")
+	updated = soup.find("p", class_ = "publish-date").getText().strip()
+
+	# find all rows
+	rows = table.findAll('tr')
+
+	# init row text array
+	row_text_array = []
+
+	# loop through rows and add row text to array
+	for row in rows[1:]:
+		row_text = []
+		# loop through the elements
+		for row_element in row.findAll(['th', 'td']):
+			# append the array with the elements inner text
+			row_text.append(row_element.text.replace('\n', '').strip())
+		# append the text array to the row text array
+		row_text_array.append(row_text)
+
+
+	with open("count.csv", "a") as f:
+		wr = csv.writer(f)
+		# loop through each row array
+		for row_text_single in row_text_array:
+			wr.writerow(row_text_single + [key, updated])
+
+	driver.get("http://www9.health.gov.au/cda/source/rpt_4_sel.cfm")
+	time.sleep(2)
+
+
+with open("rate.csv", "w") as f:
+    wr = csv.writer(f)
+    wr.writerow(col_headings)
+
+for key in causeList:
+	report = driver.find_element_by_id("REPORT_OPTION_2")
+	report.click()
+	elem = driver.find_element_by_name("CAUSE")
+	elem.send_keys(causeList[key])
+	button = driver.find_element_by_name("CTIME1")
+	button.click()
+	time.sleep(3)
+
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	#table = driver.find_element_by_css_selector("#content > div:nth-child(1) > div > div > table")
+	table = soup.find('table')
+	#updated = driver.find_elements_by_class_name("publish-date")
+	updated = soup.find("p", class_ = "publish-date").getText().strip()
+
+	# find all rows
+	rows = table.findAll('tr')
+
+	# init row text array
+	row_text_array = []
+
+	# loop through rows and add row text to array
+	for row in rows[1:]:
+		row_text = []
+		# loop through the elements
+		for row_element in row.findAll(['th', 'td']):
+			# append the array with the elements inner text
+			row_text.append(row_element.text.replace('\n', '').strip())
+		# append the text array to the row text array
+		row_text_array.append(row_text)
+
+
+	with open("rate.csv", "a") as f:
+		wr = csv.writer(f)
+		# loop through each row array
+		for row_text_single in row_text_array:
+			wr.writerow(row_text_single + [key, updated])
+
+	driver.get("http://www9.health.gov.au/cda/source/rpt_4_sel.cfm")
+	time.sleep(2)
